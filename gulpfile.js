fcdae2acc147c0cc63165c2bc6483b3f9622a8a8
valerio.versace <valerio.versace@soluzionifutura.it>
2017-03-18 07:20:21 +0100

    fatto workflow di build completo

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,17 @@ const babel = require('gulp-babel')
 gulp.task('build', () =>
   gulp.src('./public/static/app.js')
     .pipe(babel({ plugins: ['transform-async-to-generator'], presets: ['babili'] }))
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest('./build/static'))
 )
 
-gulp.task('default', ['build'], () => { console.log('Build finished')})
+gulp.task('moveApi', () =>
+  gulp.src('./public/api/*')
+    .pipe(gulp.dest('./build/api'))
+)
+
+gulp.task('moveIndex', () =>
+  gulp.src('./public/index.html')
+    .pipe(gulp.dest('./build'))
+)
+
+gulp.task('default', ['build', 'moveApi', 'moveIndex'], () => { console.log('Build finished')})

c0828870ab14aa203192de66fdb9d1be28fef08c
valerio.versace <valerio.versace@soluzionifutura.it>
2017-03-18 07:05:57 +0100

    sostituito polyfill con versione minificata; caricamento polyfill condizionale; sostituiti video.js e video.js.css con versioni minificate; pulito gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,35 +2,11 @@
 
 const gulp = require('gulp')
 const babel = require('gulp-babel')
-const del = require('del')
-const concat = require('gulp-concat')
-const polyfiller = require('gulp-polyfiller')
 
-gulp.task('buildPolyfills', () =>
-  polyfiller
-    .bundle(['Fetch'])
-    .pipe(gulp.dest('build'))
-)
-
-gulp.task('buildApp', () =>
+gulp.task('build', () =>
   gulp.src('./public/static/app.js')
-    .pipe(babel({
-      plugins: [
-        'babel-plugin-transform-es2017-object-entries',
-        'transform-async-to-generator'
-      ]
-    }))
+    .pipe(babel({ plugins: ['transform-async-to-generator'] }))
     .pipe(gulp.dest('build'))
 )
 
-gulp.task('concatFiles', ['buildPolyfills', 'buildApp'], () =>
-  gulp.src(['./build/polyfills.js', './build/app.js'])
-    .pipe(concat('app.js', { newLine: ';' }))
-    .pipe(gulp.dest('build'))
-)
-
-gulp.task('cleanPolyfills', ['concatFiles'], callback => {
-  del(['./build/polyfills.js'], callback)
-})
-
-gulp.task('default', ['cleanPolyfills'], () => { console.log('Build finished')})
+gulp.task('default', ['build'], () => { console.log('Build finished')})
--- public/static/app.js
+++ public/static/app.js
@@ -1,127 +1,138 @@
 (() => {
   'use strict'
 
-  const root = document.getElementById('root')
-  const initialUrl = document.location.pathname
-  let quotesCache = []
-  const quotesUrl = 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=10'
-  window.history.replaceState({state: 'initialState'}, '', initialUrl)
-  window.onpopstate = (e) => { goToUrl(e.target.location.pathname) }
-
-  goToUrl(initialUrl)
-
-  function goToUrl(url) {
-    if (url === '/') getIndexPage()
-    else getSingleVideoPage(url)
+  if (window.Promise && window.fetch && Object.entries) { main() }
+  else {
+    const polyfills = document.createElement('script')
+    polyfills.src = 'https://cdn.polyfill.io/v2/polyfill.min.js?features=Object.entries,Promise,fetch'
+    polyfills.onload = () => { main() }
+    polyfills.onerror = () => { main(new Error('Failed to load polyfills')) }
+    document.head.appendChild(polyfills)
   }
+  
+  function main(e) {
+    const root = document.getElementById('root')
+    const initialUrl = document.location.pathname
+    let quotesCache = []
+    const quotesUrl = 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=10'
+    window.history.replaceState({state: 'initialState'}, '', initialUrl)
+    window.onpopstate = (e) => { goToUrl(e.target.location.pathname) }
 
-  function clearPage() { while (root.hasChildNodes()) root.removeChild(root.firstChild) }
+    goToUrl(initialUrl)
 
-  function loadElement(element) { return root.appendChild(element) }
+    function goToUrl(url) {
+      if (url === '/') getIndexPage()
+      else getSingleVideoPage(url)
+    }
 
-  async function getIndexPage() { try {
-    clearPage()
-    loadElement(document.createElement('p')).innerHTML = 'Loading...'
-    const titlePromise = quotesCache.length ?
-      Promise.resolve(new Response(new Blob([JSON.stringify(quotesCache)], {type: 'application/json'}))) :
-      fetch(quotesUrl, { cache: 'no-cache' })
-    const [indexJSON, titleJSON] = await Promise.all([
-      fetch('./api/index.json').then(response => {
+    function clearPage() { while (root.hasChildNodes()) root.removeChild(root.firstChild) }
+
+    function loadElement(element) { return root.appendChild(element) }
+
+    async function getIndexPage() { try {
+      clearPage()
+      loadElement(document.createElement('p')).innerHTML = 'Loading...'
+      const titlePromise = quotesCache.length ?
+        Promise.resolve(new Response(new Blob([JSON.stringify(quotesCache)], {type: 'application/json'}))) :
+        fetch(quotesUrl, { cache: 'no-cache' })
+      const [indexJSON, titleJSON] = await Promise.all([
+        fetch('./api/index.json').then(response => {
+          if (response.status >= 400) throw response.status
+          return response.json()
+        }),
+        titlePromise.then(response => response.json())
+      ])
+      if (!quotesCache.length) quotesCache = titleJSON
+      const currentQuote = quotesCache[Math.floor(Math.random() * quotesCache.length)]
+      quotesCache = quotesCache.slice(0, quotesCache.indexOf(currentQuote)).concat(quotesCache.slice(quotesCache.indexOf(currentQuote) +1, quotesCache.length))
+      const { content: quote, title: author } = currentQuote
+      const div = document.createElement('div')
+      if (quote) {
+        div.appendChild(document.createElement('h1')).innerHTML = 'Quote of the day'
+        div.appendChild(document.createElement('h2')).innerHTML = quote
+        if (author) div.appendChild(document.createElement('h3')).innerHTML = author
+      } else { div.appendChild(document.createElement('h1')).innerHTML = 'No quote today :(' }
+      indexJSON.data.forEach(videoDataObject => {
+        div.appendChild(createGoToVideoButton('/' + videoDataObject.id, videoDataObject.poster))
+      })
+      clearPage()
+      loadElement(div)
+      if (quotesCache.length <3) quotesCache = await (await fetch(quotesUrl, { cache: 'no-cache' })).json()
+    } catch (e) { getErrorPage(e) } }
+
+    async function getSingleVideoPage(url) { try {
+      clearPage()
+      loadElement(document.createElement('p')).innerHTML = 'Loading...'
+      const videoObject = await fetch(`./api${url}.json`).then(response => {
         if (response.status >= 400) throw response.status
         return response.json()
-      }),
-      titlePromise.then(response => response.json())
-    ])
-    if (!quotesCache.length) quotesCache = titleJSON
-    const currentQuote = quotesCache[Math.floor(Math.random() * quotesCache.length)]
-    quotesCache = quotesCache.slice(0, quotesCache.indexOf(currentQuote)).concat(quotesCache.slice(quotesCache.indexOf(currentQuote) +1, quotesCache.length))
-    const { content: quote, title: author } = currentQuote
-    const div = document.createElement('div')
-    if (quote) {
-      div.appendChild(document.createElement('h1')).innerHTML = 'Quote of the day'
-      div.appendChild(document.createElement('h2')).innerHTML = quote
-      if (author) div.appendChild(document.createElement('h3')).innerHTML = author
-    } else { div.appendChild(document.createElement('h1')).innerHTML = 'No quote today :(' }
-    indexJSON.data.forEach(videoDataObject => {
-      div.appendChild(createGoToVideoButton('/' + videoDataObject.id, videoDataObject.poster))
-    })
-    clearPage()
-    loadElement(div)
-    if (quotesCache.length <3) quotesCache = await (await fetch(quotesUrl, { cache: 'no-cache' })).json()
-  } catch (e) { getErrorPage(e) } }
+      })
+      const div = document.createElement('div')
+      div.appendChild(document.createElement('h1')).innerHTML = url.substring(1)
+      div.appendChild(createVideoComponent(videoObject.data))
+      div.appendChild(createGoToIndexButton())
+      clearPage()
+      loadElement(div)
+      if (window.videojs) window.videojs(document.getElementById(videoObject.data.id))
+    } catch(e) { getErrorPage(e) } }
 
-  async function getSingleVideoPage(url) { try {
-    clearPage()
-    loadElement(document.createElement('p')).innerHTML = 'Loading...'
-    const videoObject = await fetch(`./api${url}.json`).then(response => {
-      if (response.status >= 400) throw response.status
-      return response.json()
-    })
-    const div = document.createElement('div')
-    div.appendChild(document.createElement('h1')).innerHTML = url.substring(1)
-    div.appendChild(createVideoComponent(videoObject.data))
-    div.appendChild(createGoToIndexButton())
-    clearPage()
-    loadElement(div)
-    if (window.videojs) window.videojs(document.getElementById(videoObject.data.id))
-  } catch(e) { getErrorPage(e) } }
+    function getErrorPage(error) {
+      clearPage()
+      const div = document.createElement('div')
+      div.appendChild(document.createElement('h1')).innerHTML = error
+      div.appendChild(createGoToIndexButton())
+      loadElement(div)
+    }
 
-  function getErrorPage(error) {
-    clearPage()
-    const div = document.createElement('div')
-    div.appendChild(document.createElement('h1')).innerHTML = error
-    div.appendChild(createGoToIndexButton())
-    loadElement(div)
-  }
+    function createVideoComponent(videoData) {
+      const video = document.createElement('video')
+      video.setAttribute('id', videoData.id)
+      video.setAttribute('poster', videoData.poster)
+      video.setAttribute('width', '640')
+      video.setAttribute('height', '264')
+      video.setAttribute('controls', null)
+      video.setAttribute('preload', 'auto')
+      Object.entries(videoData.sources).forEach(([key, value]) => {
+        const child = document.createElement('source')
+        child.setAttribute('src', value)
+        child.setAttribute('type', key)
+        video.appendChild(child)
+      })
+      video.classList.add('video-js', 'vjs-default-skin', 'vjs-big-play-centered')
+      video.style.maxWidth = '100%'
+      video.style.maxHeight = '264'
+      return video
+    }
 
-  function createVideoComponent(videoData) {
-    const video = document.createElement('video')
-    video.setAttribute('id', videoData.id)
-    video.setAttribute('poster', videoData.poster)
-    video.setAttribute('width', '640')
-    video.setAttribute('height', '264')
-    video.setAttribute('controls', null)
-    video.setAttribute('preload', 'auto')
-    Object.entries(videoData.sources).forEach(([key, value]) => {
-      const child = document.createElement('source')
-      child.setAttribute('src', value)
-      child.setAttribute('type', key)
-      video.appendChild(child)
-    })
-    video.classList.add('video-js', 'vjs-default-skin', 'vjs-big-play-centered')
-    video.style.maxWidth = '100%'
-    video.style.maxHeight = '264'
-    return video
-  }
+    function createGoToIndexButton() {
+      const button = document.createElement('button')
+      button.innerHTML = 'Go to index'
+      button.addEventListener('click', (e) => {
+        e.preventDefault()
+        goToUrl('/')
+        window.history.pushState(null, '', '/')
+      })
+      return button
+    }
 
-  function createGoToIndexButton() {
-    const button = document.createElement('button')
-    button.innerHTML = 'Go to index'
-    button.addEventListener('click', (e) => {
-      e.preventDefault()
-      goToUrl('/')
-      window.history.pushState(null, '', '/')
-    })
-    return button
-  }
-
-  function createGoToVideoButton(url, poster) {
-    const videoThumb = document.createElement('img')
-    videoThumb.setAttribute('src', poster)
-    videoThumb.setAttribute('width', '640')
-    videoThumb.setAttribute('height', 'auto')
-    videoThumb.style.maxWidth = '100%'
-    videoThumb.style.maxHeight = '264'
-    const a = document.createElement('a')
-    a.setAttribute('href', url)
-    a.addEventListener('click', (e) => {
-      e.preventDefault()
-      goToUrl(url)
-      window.history.pushState(null, '', url)
-    })
-    const div = document.createElement('div')
-    div.appendChild(a)
-      .appendChild(videoThumb)
-    return div
+    function createGoToVideoButton(url, poster) {
+      const videoThumb = document.createElement('img')
+      videoThumb.setAttribute('src', poster)
+      videoThumb.setAttribute('width', '640')
+      videoThumb.setAttribute('height', 'auto')
+      videoThumb.style.maxWidth = '100%'
+      videoThumb.style.maxHeight = '264'
+      const a = document.createElement('a')
+      a.setAttribute('href', url)
+      a.addEventListener('click', (e) => {
+        e.preventDefault()
+        goToUrl(url)
+        window.history.pushState(null, '', url)
+      })
+      const div = document.createElement('div')
+      div.appendChild(a)
+        .appendChild(videoThumb)
+      return div
+    }
   }
 })()
